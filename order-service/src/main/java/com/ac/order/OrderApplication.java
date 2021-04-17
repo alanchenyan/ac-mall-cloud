46e8b9b0b77c33d48ee130a19b9dccfd3e9d0868
AlanChen <chenyan900520@126.com>
2021-04-17 18:14:39 +0800

    ac

--- order-service/src/main/java/com/ac/order/OrderApplication.java
+++ order-service/src/main/java/com/ac/order/OrderApplication.java
@@ -2,6 +2,7 @@ package com.ac.order;
         
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
