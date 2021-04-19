30f4dcd00b11e2b671cbd9c659c5e8cb88deff8d
AlanChen <chenyan900520@126.com>
2021-04-19 10:55:51 +0800

    ac

--- product-service/src/main/java/com/ac/product/ProductApplication.java
+++ product-service/src/main/java/com/ac/product/ProductApplication.java
@@ -2,13 +2,15 @@ package com.ac.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author Alan Chen
  * @description
  * @date 2020/10/15
  */
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ProductApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/ac/user/UserApplication.java
+++ user-service/src/main/java/com/ac/user/UserApplication.java
@@ -2,13 +2,15 @@ package com.ac.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author Alan Chen
  * @description
  * @date 2020/10/15
  */
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class UserApplication {
 
     public static void main(String[] args) {
