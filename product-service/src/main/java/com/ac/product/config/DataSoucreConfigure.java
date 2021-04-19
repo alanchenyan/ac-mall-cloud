a088395b6357aa412781d1ec0fc99279babcb512
AlanChen <chenyan900520@126.com>
2021-04-19 10:12:42 +0800

    ac

---
+++ product-service/src/main/java/com/ac/product/config/DataSoucreConfigure.java
@@ -0,0 +1,24 @@
+package com.ac.product.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @author Alan Chen
+ * @description
+ * @date 2021/4/19
+ */
+@Configuration
+public class DataSoucreConfigure {
+
+    @Bean
+    @Primary
+    public DataSourceProxy dataSourceProxy(DataSourceProperties properties){
+        HikariDataSource dataSource = properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        return new DataSourceProxy(dataSource);
+    }
+}
